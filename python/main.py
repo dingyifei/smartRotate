17a6ff426d7a142c9e7f89bf3fbb8285492e880e
Yifei Ding <dingyifeiair@gmail.com>
2020-05-27 14:14:00 -0700

    improvement

--- python/main.py
+++ python/main.py
@@ -14,11 +14,29 @@ either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 """
 from tkinter import *
+import rotate
+
+
+def cmd_ccw():
+
+    return 0
+
+
+def cmd_cw():
+    return 0
+
+
+def cmd_rst():
+    return 0
+
+
+def cmd_bind():
+    return 0
 
 
 def main():
     """
-
+    This is the GUI for rotating display, etc
     """
     main_window = Tk()
     main_window.title("Smart Rotate")
@@ -32,33 +50,33 @@ def main():
     orientation = StringVar()
 
     label_x_rotation = Label(main_frame, text="X Rotation:")
-    label_x_rotation.grid(column=0, row=0, sticky=(W, N))
+    label_x_rotation.grid(column=0, row=0, sticky=W)
     label_y_rotation = Label(main_frame, text="Y Rotation:")
-    label_y_rotation.grid(column=0, row=1, sticky=(W, N))
+    label_y_rotation.grid(column=0, row=1, sticky=W)
     label_z_rotation = Label(main_frame, text="Z Rotation:")
-    label_z_rotation.grid(column=0, row=2, sticky=(W, N))
+    label_z_rotation.grid(column=0, row=2, sticky=W)
     label_orientation = Label(main_frame, text="Orientation:")
-    label_orientation.grid(column=0, row=3, sticky=(W, N))
+    label_orientation.grid(column=0, row=3, sticky=W)
     menubutton_display = Menubutton(main_frame, text="Displays")
-    menubutton_display.grid(column=0, row=4, sticky=W)
+    menubutton_display.grid(column=0, row=4, sticky=W, columnspan=2)
 
     label_x_reading = Label(main_frame, textvariable=x_rotation)
-    label_x_reading.grid(column=1, row=0, sticky=(W, N))
+    label_x_reading.grid(column=1, row=0, sticky=W)
     label_y_reading = Label(main_frame, textvariable=y_rotation)
-    label_y_reading.grid(column=1, row=1, sticky=(W, N))
+    label_y_reading.grid(column=1, row=1, sticky=W)
     label_z_reading = Label(main_frame, textvariable=z_rotation)
-    label_z_reading.grid(column=1, row=2, sticky=(W, N))
+    label_z_reading.grid(column=1, row=2, sticky=W)
     label_orientation = Label(main_frame, textvariable=orientation)
-    label_orientation.grid(column=1, row=3, sticky=(W, N))
+    label_orientation.grid(column=1, row=3, sticky=W)
 
-    btn_ccw = Button(main_frame, text="Counter Clockwise Turn")
-    btn_ccw.grid(column=2, row=0)
-    btn_cw = Button(main_frame, text="Clockwise Turn")
-    btn_cw.grid(column=2, row=1)
-    btn_rst = Button(main_frame, text="Reset")
-    btn_rst.grid(column=2, row=3)
-    btn_bind = Button(main_frame, text="Bind")
-    btn_bind.grid(column=2, row=4)
+    btn_ccw = Button(main_frame, text="CCW Turn", command=cmd_ccw)
+    btn_ccw.grid(column=2, row=0, sticky=EW, ipadx=5)
+    btn_cw = Button(main_frame, text="CW Turn", command=cmd_cw)
+    btn_cw.grid(column=2, row=1, sticky=EW, ipadx=5)
+    btn_rst = Button(main_frame, text="Reset", command=cmd_rst)
+    btn_rst.grid(column=2, row=2, sticky=EW, ipadx=5)
+    btn_bind = Button(main_frame, text="Bind", command=cmd_bind)
+    btn_bind.grid(column=2, row=3, sticky=EW, ipadx=5)
 
     main_window.mainloop()
 
--- python/rotate.py
+++ python/rotate.py
@@ -13,51 +13,99 @@ either express or implied.
 
 See the License for the specific language governing permissions and limitations under the License.
 """
-import win32api, win32con
+from sys import platform
 import pywintypes
+import win32api
+import win32con
+
+if platform.startswith('win32'):
+    platform = "windows"
 
 
 # useful doc https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/
 
 
-def change_orientation(display: win32api.PyDISPLAY_DEVICEType, settings: list):
-    win32api.ChangeDisplaySettingsEx(display.DeviceName, settings, 4)  # 4 is a magic number  I don't know why it worked
-    return 0
+def win_apply_monitor_settings(monitor: win32api.PyDISPLAY_DEVICEType, settings: list):
+    """
+    apply the DEVMODE to a known display
+    :param monitor: a PyDISPLAY_DEVICEType object that have been confirmed to be a monitor
+    :param settings: DEVMODE object
+    """
+    dm_size = 4  # private driver data following in the DEVMODE, not sure how to do it
+    win32api.ChangeDisplaySettingsEx(monitor.DeviceName, settings, dm_size, None, win32con.CDS_GLOBAL)
 
 
-def get_display_settings(display: win32api.PyDISPLAY_DEVICEType):
-    return win32api.EnumDisplaySettings(display.DeviceName)
+def win_get_monitor_settings(monitor: win32api.PyDISPLAY_DEVICEType):
+    """
+    Only physical monitor can be accepted because of some magical reason
+    :param monitor: a DISPLAY_DEVICE object that have been confirmed to be a monitor
+    :return: the DEVMODE object of the monitor object
+    """
+    return win32api.EnumDisplaySettings(monitor.DeviceName)
 
 
-def get_displays():
+def win_get_displays() -> list:
+    """
+    enumerate all the displays connected at the time.
+    :return: list of PyDISPLAY_DEVICEType object
+    """
     displays = []
     i = 0
 
     while True:
         try:
-            adapter = win32api.EnumDisplayDevices(None, i)
-            displays.append(adapter)
-        except pywintypes.error:
+            display = win32api.EnumDisplayDevices(None, i)
+            displays.append(display)
+        except pywintypes.error:  # if a error happen, it means we find something that's not a object.
             break
         i += 1
     return displays
 
 
-def get_displays_info(displays: list):
-    # display_count = win32api.GetSystemMetrics(80)
-    display_infos = []
+def win_get_display_info(display: win32api.PyDISPLAY_DEVICEType) -> win32api.PyDISPLAY_DEVICEType:
+    """
+    obtain detail information of a display, raise pywintypes.error when a display is not a physical monitor
+    :param display: a PyDISPLAY_DEVICEType object from EnumDisplayDevices
+    :return: return a PyDISPLAY_DEVICEType with detailed information of display
+    """
+    return win32api.EnumDisplayDevices(display.DeviceName, 0)  # the 0 here is a magic number, take caution
 
-    for display in displays:
-        try:
-            display_info = win32api.EnumDisplayDevices(display.DeviceName, 0)  # the 0 here is a magic number, take caution
-            displays.append(display_info)
-        except pywintypes.error:
-            continue
-    return display_infos
+
+def win_is_monitor(display: win32api.PyDISPLAY_DEVICEType):
+    """
+
+    :param display:
+    :return:
+    """
+    try:
+        win_get_display_info(display)
+    except pywintypes.error:
+        return False
+    return True
+
+
+def win_change_orientation(monitor: win32api.PyDISPLAY_DEVICEType, orientation):
+    """
+    apply a orientation setting that can be win32con.DMDO*
+    :param monitor: the PyDISPLAY_DEVICEType object
+    :param orientation: win32con.DMDO_* object or 0-3
+    """
+    settings = win_get_monitor_settings(monitor)
+    settings.DisplayOrientation = orientation
+    win_apply_monitor_settings(monitor, settings)
 
 
 def main():
-    display_list = get_displays()
+    """
+        it's just for testing purposes
+    """
+    # just random code for testing
+    display_list = win_get_displays()
+    y = win_get_display_info(display_list[0])
+    x = win_get_monitor_settings(display_list[0])
+    if win32con.DMDO_90 == x.DisplayOrientation:
+        print("yes")
+    print(x)
 
 
 if __name__ == "__main__":
